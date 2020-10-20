1482f78427cc74a7278fd411070c8be2b771728d
Eric Wajnrajch <ericwaje03@gmail.com>
2020-10-19 21:11:27 -0300

    cambios

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//si toco algun boton como por ejemplo contacto, el menu hamburguesa tiene que cerrarse
+
+
+const pulsar = document.querySelector('.boton-home');
+
+pulsar.addEventListener('click', function() {
+    console.log('se pulso el titulo');
+});

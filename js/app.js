5c676a402e99fbd1f4715ce8b6b78fc579d7d1e1
Eric Wajnrajch <ericwaje03@gmail.com>
2021-01-20 13:21:26 -0300

    app

--- js/app.js
+++ js/app.js
@@ -1,8 +1,44 @@
-//si toco algun boton como por ejemplo contacto, el menu hamburguesa tiene que cerrarse
+window.addEventListener('DOMContentLoaded', function () {
+  // get the form elements defined in your form HTML above
 
+  var form = document.getElementById('form');
+  //var button = document.getElementById('my-form-button');
+  var status = document.getElementById('status');
 
-const pulsar = document.querySelector('.boton-home');
+  // Success and Error functions for after the form is submitted
 
-pulsar.addEventListener('click', function() {
-    console.log('se pulso el titulo');
+  function success() {
+    form.reset();
+    //button.style = 'display: none ';
+    status.innerHTML = 'Se ha enviado con éxito!';
+  }
+
+  function error() {
+    status.innerHTML = 'Ocurrió un error al enviarlo';
+  }
+
+  // handle the form submission event
+
+  form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
 });
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
